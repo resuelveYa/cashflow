7c293b1861010a06f5dbd3778db56010184037cb
Felipe Salazar <felipeslzar@gmail.com>
2025-12-22 20:47:12 -0300

    feat: enable cross-subdomain SSO with Clerk domain config

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider
       publishableKey={clerkPubKey}
+      domain=".resuelveya.cl"
       afterSignOutUrl={ENV.CLERK.AFTER_SIGN_IN_URL}
       signInUrl={ENV.CLERK.SIGN_IN_URL}
       signUpUrl={ENV.CLERK.SIGN_UP_URL}
