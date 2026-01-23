9b08128eafd23e1554ef5e829ab95a3b648374db
Felipe Salazar <felipeslzar@gmail.com>
2026-01-23 10:41:03 -0300

    fix: error in login

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,10 @@ import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    storageKey: 'sb-auth-token',
+  }
+})
