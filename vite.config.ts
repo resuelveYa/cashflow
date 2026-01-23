7d28c65f891ba897e798d17faa3fe6cb293fe7ab
Felipe Salazar <felipeslzar@gmail.com>
2026-01-23 13:59:36 -0300

    fix: removed base path for subdomain migration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: '/cashflow', // 👈 ahora configurado para sub-ruta en producción
+  base: '/', // 👈 Ahora en su propio subdominio, no necesita sub-ruta
   resolve: {
     alias: {
       '@': '/src'
